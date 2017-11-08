da434a54d8d0b074ad70b7f9e7cb37c51ccc3527
Bailey <33033378+bp131432@users.noreply.github.com>
2017-11-08 10:32:36 -0600

    Created Salary form

---
+++ Salary Calculator form/Salary Calculator form/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtAnnualSalary = New System.Windows.Forms.TextBox()
+        Me.txtPayPeriods = New System.Windows.Forms.TextBox()
+        Me.lblSalary = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(69, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Annual salary:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(60, 90)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(109, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Pay periods per year: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(60, 122)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(112, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Salary per pay period: "
+        '
+        'txtAnnualSalary
+        '
+        Me.txtAnnualSalary.Location = New System.Drawing.Point(203, 45)
+        Me.txtAnnualSalary.Name = "txtAnnualSalary"
+        Me.txtAnnualSalary.Size = New System.Drawing.Size(100, 20)
+        Me.txtAnnualSalary.TabIndex = 3
+        '
+        'txtPayPeriods
+        '
+        Me.txtPayPeriods.Location = New System.Drawing.Point(203, 82)
+        Me.txtPayPeriods.Name = "txtPayPeriods"
+        Me.txtPayPeriods.Size = New System.Drawing.Size(100, 20)
+        Me.txtPayPeriods.TabIndex = 4
+        '
+        'lblSalary
+        '
+        Me.lblSalary.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSalary.Location = New System.Drawing.Point(203, 121)
+        Me.lblSalary.Name = "lblSalary"
+        Me.lblSalary.Size = New System.Drawing.Size(100, 23)
+        Me.lblSalary.TabIndex = 5
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(161, 212)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 6
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblStatus.Location = New System.Drawing.Point(1, 319)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(400, 23)
+        Me.lblStatus.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(400, 342)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblSalary)
+        Me.Controls.Add(Me.txtPayPeriods)
+        Me.Controls.Add(Me.txtAnnualSalary)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtAnnualSalary As System.Windows.Forms.TextBox
+    Friend WithEvents txtPayPeriods As System.Windows.Forms.TextBox
+    Friend WithEvents lblSalary As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+
+End Class
